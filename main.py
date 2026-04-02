a6a4f4a6bd997fc2670478e298dca1f30cb1df6c
Elias Switzer <65833729+eliasswitzer@users.noreply.github.com>
2026-04-02 16:37:04 -0400

    added more arguments

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('-s', '--seed', metavar="seed", type=int, required=True, help="The random seed for reproducibility.")
 
+# PSO Parameters
+parser.add_argument('-w', metavar='w', type=float, required=False, default=0.729, help="The inertia term weight for PSO algorithm.")
+parser.add_argument('-c1', metavar='c1', type=float, required=False, default=1.49445, help="The cognitive acceleration coefficient for PSO algorithm.")
+parser.add_argument('-c2', metavar='c2', type=float, required=False, default=1.49445, help="The social acceleration coefficient for PSO algorithm.")
+
+# Datasets
+parser.add_argument('--dataset', metavar='dataset', type=str, required=False, default="chest", choices=["chest, retina, blood"], help="The dataset the MLP will be trained on.")
+
 args = parser.parse_args()
+
+print(f"Args: {args}")
 
 # Set random seed
 np.random.seed(args.seed)
@@ -19,10 +29,19 @@ torch.manual_seed(args.seed)
 g = torch.Generator().manual_seed(args.seed)
 
 # Load dataset
-from medmnist import ChestMNIST
-train_dataset = ChestMNIST(split='train', transform=transforms.ToTensor(), download=True)
-val_dataset = ChestMNIST(split='val', transform=transforms.ToTensor(), download=True)
-test_dataset = ChestMNIST(split='test', transform=transforms.ToTensor(), download=True)
+from medmnist import ChestMNIST, RetinaMNIST, BloodMNIST
+if args.dataset == 'chest':
+    train_dataset = ChestMNIST(split='train', transform=transforms.ToTensor(), download=True)
+    val_dataset = ChestMNIST(split='val', transform=transforms.ToTensor(), download=True)
+    test_dataset = ChestMNIST(split='test', transform=transforms.ToTensor(), download=True)
+elif args.dataset == 'retina':
+    train_dataset = RetinaMNIST(split='train', transform=transforms.ToTensor(), download=True)
+    val_dataset = RetinaMNIST(split='val', transform=transforms.ToTensor(), download=True)
+    test_dataset = RetinaMNIST(split='test', transform=transforms.ToTensor(), download=True)
+else:
+    train_dataset = BloodMNIST(split='train', transform=transforms.ToTensor(), download=True)
+    val_dataset = BloodMNIST(split='val', transform=transforms.ToTensor(), download=True)
+    test_dataset = BloodMNIST(split='test', transform=transforms.ToTensor(), download=True)
 
 num_classes = len(train_dataset.info['label'])
 input_dim = int(np.prod(train_dataset[0][0].shape))
@@ -42,7 +61,7 @@ search_bounds = [
 #print(_complexity_score(pso1.particles[1].get_network_params()))
 #print(_get_complexity(pso1.particles[1].get_network_params()))
 
-pso = PSO(num_particles=10, search_bounds=search_bounds)
+pso = PSO(num_particles=10, search_bounds=search_bounds, w=args.w, c1=args.c1, c2=args.c2)
 best_position = pso.optimize(3, search_bounds, train_dataset, val_dataset, test_dataset, input_dim, num_classes, g)
 
 best = Particle(search_bounds)
