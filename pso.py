9950d0eaf81f0e0abc42df429f7404dc53469978
ks21xf <ks21xf@brocku.ca>
2026-04-04 18:28:06 -0400

    changed to a better evaluation metric, and added stronger weight for positive labels in loss function
    switched from accuracy to f1 score
    added stronger weight to positive labels so model doesnt pick 0 all the time

--- pso.py
+++ pso.py
@@ -108,7 +108,7 @@ class PSO:
       if no_improvement_count >= patience:
          print("Fitness improvement has stagnated, stopping early!")
          break
-      
+
       # Early Stopping: Checking particle distance
       positions = np.array([p.position for p in self.particles])
       swarm_spread = np.mean(np.std(positions, axis=0))
@@ -156,29 +156,44 @@ class PSO:
       curr_complexity = self._get_complexity(network_params)
       return (curr_complexity/highest_score)
 
-"""this function gets loss and accuracy with a given dataset and labels"""
-def eval_model(x_set,y_set,model):
+"""this function gets f1_score with a given dataset and labels"""
+def f1_score(x_set,y_set,model):
   with torch.no_grad(): #this disables gradient calculations
 
     #sigmoid on each label to get the predictions the model has for each label?
-    crit = nn.Sigmoid()
-    outputs = crit(model(x_set))
+    #crit = nn.Sigmoid()
+    outputs = model(x_set)
 
-    #conv to numpy and calculate accuracy and loss
+    #conv to numpy
     numpy_pred = outputs.cpu().numpy()
     y_valid_numpy = y_set.cpu().numpy()
 
-    numpy_pred = np.where(numpy_pred > 0.5 ,1,0)
+    numpy_pred = np.where(numpy_pred > 0 ,1,0)
 
-    #boolean array of whether the model guessed correctly for each label, for each datapoint
-    correct_labels = numpy_pred == y_valid_numpy
+    #get tp
+    right_guesses = np.where((numpy_pred == y_valid_numpy),1,0) #get all instances where predictions = actual labels
+    tp = np.logical_and(right_guesses,y_valid_numpy) #get the predictions where they guess positive (1) label correct
+    tp = np.sum(tp.astype(int),axis=1)
 
-    #this figures out how accurate the models guess for an individual unit was
-    acc_per_unit = correct_labels.astype(int).sum(axis=1)/len(correct_labels[0])
+    #get fn
+    tn = np.logical_not(np.logical_or(numpy_pred,y_valid_numpy)) #get pred where neg (0) label was guessed correct. this is where y_pred=0 and labels = 0, so use nor gate
+    tn = np.sum(tn.astype(int),axis=1)
 
-    #average accuracy (of all datapoints
-    acc = acc_per_unit.sum()/len(acc_per_unit)
-    return acc
+    #false positive - where model guesses positive but its actually negative
+    wrong_guesses = np.logical_xor(numpy_pred,y_valid_numpy).astype(int) #instances where predictions != actual labels (1 =wrong)
+    fp = np.logical_and(wrong_guesses,numpy_pred) #if model guesses wrong (wrong=1) and pred is 1 (positive), means the label is actually 0 (neg). so when both these are 1 we have a fp
+    fp = np.sum(fp.astype(int),axis=1)
+
+    #false negative - model guesses negative but its actually positive
+    fn = np.logical_and(wrong_guesses,np.logical_not(numpy_pred)) #if model is wrong, and pred is negative, label is 1. negate the pred label then and it with wrong_guess, this will be one if fn
+    fn=  np.sum(fn.astype(int),axis=1)
+
+    #do the calculations if denoms aren't 0, or else just set to 0
+    recall = np.where((tp+fn) ==0, 0, tp/(tp+fn))
+    precision = np.where((tp+fp)==0, 0, tp/(tp+fp))
+    f1 = np.where((precision+recall) ==0, 0, 2*(precision*recall)/(precision+recall) )
+
+    return  f1.mean()
 
 # evalutate fitness of a particle by training a neural net on the parameters
 def evaluate_particle(parameters, train_dataset, val_dataset, test_dataset, input_dim, num_classes, g, epochs=5, overfitting_detection = 10):
@@ -191,7 +206,7 @@ def evaluate_particle(parameters, train_dataset, val_dataset, test_dataset, inpu
   model = MLP(input_dim=input_dim, num_classes=num_classes, num_layers=parameters['num_hidden_layers'], hidden_size=parameters['hidden_layer_size'], dropout_rate=parameters['dropout_rate']).to(device)
 
   optimizer = torch.optim.SGD(model.parameters(), lr=parameters['learning_rate'], momentum=parameters['momentum'], weight_decay=parameters['weight_decay'])
-  criterion = nn.BCEWithLogitsLoss() # BCEWithLogitsLoss is used for multi-class classification problems
+  criterion = nn.BCEWithLogitsLoss(pos_weight= torch.tensor([8])) # BCEWithLogitsLoss is used for multi-class classification problems
 
   running_mean_loss = 0 #stores average loss over minibatches, so this is mean per epoch
   count = 1 #counts minibatches
@@ -234,20 +249,23 @@ def evaluate_particle(parameters, train_dataset, val_dataset, test_dataset, inpu
   # Testing
   model.eval()
   test_loss = 0.0
-  total_acc = 0.0
+  total_eval_metric = 0.0
   with torch.no_grad():
     for images, labels in test_loader:
         images, labels = images.to(device), labels.float().to(device)
         test_loss += criterion(model(images), labels).item()
-        curr_acc = eval_model(images, labels, model)
-        total_acc += curr_acc
-        #print(curr_acc)
+        curr_eval_metric = f1_score(images, labels, model)
+        total_eval_metric+= curr_eval_metric
 
-  print(total_acc)
+        print(model(images)[0])
+        print(labels[0])
+
+  print(total_eval_metric)
   avg_test_loss = test_loss / len(test_loader)
-  avg_acc = total_acc / len(test_loader)
+  avg_eval_metric = total_eval_metric / len(test_loader)
 
-  print(f"Test Loss: {avg_test_loss:.4f} | Test Acc: {avg_acc:.4f}") #debug
+  print(f"Test Loss: {avg_test_loss:.4f} | Test Evaluation Metric (f1/acc): {avg_eval_metric:.4f}") #debug
+
 
   return test_loss / len(test_loader)
 
@@ -266,15 +284,15 @@ def penalty_function(parameters, search_bounds):
 
     return penalty * 100
 
-def weighted_sums(alpha,beta,complexity,accuracy):
-    """take a weighted sum of the accuracy and model size"""
-    """alpha, beta = weights. complexity, accuracy = individual obj. function values."""
-    return (alpha*complexity)+(beta*accuracy)
+def weighted_sums(alpha,beta,complexity,eval_metric):
+    """take a weighted sum of the eval_metric and model size"""
+    """alpha, beta = weights. complexity, eval_metric = individual obj. function values."""
+    return (alpha*complexity)+(beta*eval_metric)
 
 # TODO: Make this our multi-objective function instead of just validation loss
 def objective_function(parameters, search_bounds, train_dataset, val_dataset, test_dataset, input_dim, num_classes, generator):
   """
-  Returns fitness of a particle based on model accuracy and model complexity. Constraints values to within the search bounds and applies
+  Returns fitness of a particle based on model evaluation metric and model complexity. Constraints values to within the search bounds and applies
   a penalty to particles that go outside of those bounds.
   """
   penalty = penalty_function(parameters, search_bounds)
@@ -283,6 +301,7 @@ def objective_function(parameters, search_bounds, train_dataset, val_dataset, te
   parameter_keys = ['num_hidden_layers', 'hidden_layer_size', 'learning_rate', 'momentum', 'batch_size', 'weight_decay', 'dropout_rate']
   for parameter, (low, high) in zip(parameter_keys, search_bounds):
      clipped_parameters[parameter] = max(low, min(high, parameters[parameter])) # ensure the value of each parameter is within the search bounds
-  
+
   fitness = evaluate_particle(clipped_parameters, train_dataset, val_dataset, test_dataset, input_dim, num_classes, epochs=5, g=generator)
   return fitness + penalty
+
